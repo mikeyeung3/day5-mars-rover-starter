6a64300f85a248b3cfe607ed8fca3c8ad10a4109
Mike Yeung <yeungmi3@oocl.com>
2025-09-05 16:16:58 +0800

    fix: test naming

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void should_increase_x_given_command_M_direction_N() {
+    void should_increase_y_given_command_M_direction_N() {
         String command = "M";
 
         MarsRover marsRover = new MarsRover(0, 0, "N");
