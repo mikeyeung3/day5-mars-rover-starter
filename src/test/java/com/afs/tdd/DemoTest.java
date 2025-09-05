a4e7604316d2446a8ef6277b7206a63a0f070ec3
Mike Yeung <yeungmi3@oocl.com>
2025-09-05 16:12:49 +0800

    feat: should_increase_y_given_command_M_direction_N

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,16 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void should_increase_x_given_command_M_direction_N() {
+        String command = "M";
 
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        marsRover.execute(command);
+
+        assertEquals(1 , marsRover.getY());
     }
 }
